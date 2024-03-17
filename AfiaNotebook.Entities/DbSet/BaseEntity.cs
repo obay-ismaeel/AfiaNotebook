f113410d13d07946dd1e426bb1c688f699d30930
ObayIsm <obay.engineer@gmail.com>
2024-03-18 01:57:22 +0300

    Created User Entity

---
+++ AfiaNotebook.Entities/DbSet/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace AfiaNotebook.Entities.DbSet;
+public abstract class BaseEntity
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public int Status { get; set; } = 1;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+}
---
+++ AfiaNotebook.Entities/DbSet/User.cs
@@ -0,0 +1,11 @@
+﻿namespace AfiaNotebook.Entities.DbSet;
+public class User : BaseEntity
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string Country { get; set; }
+
+}
