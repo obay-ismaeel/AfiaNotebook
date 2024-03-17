c01f228d869e74a11b43abe4d79795ab1dae9d96
ObayIsm <obay.engineer@gmail.com>
2024-03-18 01:25:20 +0300

    Created AppDbContext

---
+++ AfiaNotebook.DataService/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AfiaNotebook.DataService.Data;
+internal class AppDbContext : IdentityDbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+}
