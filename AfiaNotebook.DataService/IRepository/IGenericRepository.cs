9387544693b828faadbc2735cc332482a6477257
ObayIsm <obay.engineer@gmail.com>
2024-03-18 22:55:22 +0300

    Created Generic Repository Class & Interface

---
+++ AfiaNotebook.DataService/IRepository/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace AfiaNotebook.DataService.IRepository;
+public interface IGenericRepository<T> where T : class
+{
+    Task<IEnumerable<T>> GetAll();
+    
+    Task<T> GetById(Guid id);
+    
+    Task<bool> Add(T entity);
+    
+    Task<bool> Upsert(T entity); // update or add if doesn't exists
+    
+    Task<bool> Delete(Guid id, string userId); 
+}
---
+++ AfiaNotebook.DataService/Repository/GenericRepository.cs
@@ -0,0 +1,45 @@
+﻿using AfiaNotebook.DataService.Data;
+using AfiaNotebook.DataService.IRepository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace AfiaNotebook.DataService.Repository;
+public class GenericRepository<T> : IGenericRepository<T> where T : class
+{
+    protected readonly ILogger _logger;
+    protected readonly AppDbContext _context;
+    protected DbSet<T> _dbSet;
+
+    public GenericRepository(AppDbContext context, ILogger logger)
+    {
+        _context = context;
+        _dbSet = _context.Set<T>();
+        _logger = logger;
+    }
+
+    public virtual async Task<bool> Add(T entity)
+    {
+        await _dbSet.AddAsync(entity);
+        return true;
+    }
+
+    public virtual async Task<bool> Delete(Guid id, string userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public virtual async Task<IEnumerable<T>> GetAll()
+    {
+        return await _dbSet.ToListAsync();
+    }
+
+    public virtual async Task<T> GetById(Guid id)
+    {
+        return await _dbSet.FindAsync(id);
+    }
+
+    public virtual async Task<bool> Upsert(T entity)
+    {
+        throw new NotImplementedException();
+    }
+}
