9ffad3195db01ef9ec45413a5f01b197777d4c87
ObayIsm <obay.engineer@gmail.com>
2024-03-18 03:50:49 +0300

    Added Incoming UserDto Class

---
+++ AfiaNotebook.Entities/Dtos/Incoming/UserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace AfiaNotebook.Entities.Dtos.Incoming;
+public class UserDto
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public string DateOfBirth { get; set; }
+    public string Country { get; set; }
+}
