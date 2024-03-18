02cab37d4a5113d1626f99270811cea333120e40
ObayIsm <obay.engineer@gmail.com>
2024-03-18 22:55:41 +0300

    Created Users Repository Class & Interface

---
+++ AfiaNotebook.DataService/IRepository/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using AfiaNotebook.Entities.DbSet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AfiaNotebook.DataService.IRepository;
+public interface IUsersRepository : IGenericRepository<User>
+{
+}
---
+++ AfiaNotebook.DataService/Repository/UserRepository.cs
@@ -0,0 +1,33 @@
+﻿using AfiaNotebook.DataService.Data;
+using AfiaNotebook.DataService.IRepository;
+using AfiaNotebook.Entities.DbSet;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AfiaNotebook.DataService.Repository;
+public class UsersRepository : GenericRepository<User>, IUsersRepository
+{
+    public UsersRepository(AppDbContext context, ILogger logger) : base(context, logger)
+    {
+    }
+
+    public override async Task<IEnumerable<User>> GetAll()
+    {
+        try
+        {
+            return await _dbSet.Where(x => x.Status == 1)
+                .AsNoTracking()
+                .ToListAsync();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "{Repo} GetAll method has generated an error", typeof(UsersRepository));
+            return new List<User>() ;
+        }
+    }
+}
