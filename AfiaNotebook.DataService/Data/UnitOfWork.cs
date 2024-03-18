fb6191b96d8fea1823368534395845e548f2c9fe
ObayIsm <obay.engineer@gmail.com>
2024-03-18 22:56:14 +0300

    Created Unit of Work Class & Interface

---
+++ AfiaNotebook.DataService/Data/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using AfiaNotebook.DataService.IConfiguration;
+using AfiaNotebook.DataService.IRepository;
+using AfiaNotebook.DataService.Repository;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AfiaNotebook.DataService.Data;
+internal class UnitOfWork : IUnitOfWork, IDisposable
+{
+    private readonly AppDbContext _context;
+    private readonly ILogger _logger;
+    public IUsersRepository Users {get; private set;}
+
+    public UnitOfWork(AppDbContext context, ILoggerFactory loggerFactory)
+    {
+        _context = context;
+        _logger = loggerFactory.CreateLogger("db_logs");
+        Users = new UsersRepository(_context, _logger);
+    }
+
+    public async Task CompleteAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+
+    public void Dispose()
+    {
+        _context.Dispose();
+    }
+}
---
+++ AfiaNotebook.DataService/IConfiguration/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿using AfiaNotebook.DataService.IRepository;
+
+namespace AfiaNotebook.DataService.IConfiguration;
+public interface IUnitOfWork
+{
+    IUsersRepository Users { get; }
+
+    Task CompleteAsync();
+}
