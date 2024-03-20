5b9d04847637351107c05a595321f67bf97884a9
ObayIsm <obay.engineer@gmail.com>
2024-03-21 01:12:17 +0300

    Added Refresh Token Entity

---
+++ AfiaNotebook.Entities/DbSet/RefreshToken.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AfiaNotebook.Entities.DbSet;
+public class RefreshToken : BaseEntity
+{
+    public string UserId { get; set; } // user id when logged in
+    public string Token { get; set; }
+    public string JwtId { get; set; } // id generated when a jwt token is created
+    public bool IsUsed { get; set; } // to make sure r.token is used only once
+    public bool IsRevoked { get; set; } // make sure they are valid
+    public DateTime ExpiryDate { get; set; }
+    
+    [ForeignKey(nameof(UserId))]
+    public IdentityUser User { get; set; }
+}
