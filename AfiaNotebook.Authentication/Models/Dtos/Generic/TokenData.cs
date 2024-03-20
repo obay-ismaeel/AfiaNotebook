133a1dcbd66f934f582ec49d54b343f8c55205b9
ObayIsm <obay.engineer@gmail.com>
2024-03-21 01:16:23 +0300

    Updated Dtos to Hold Refresh Token Property

---
+++ AfiaNotebook.Authentication/Models/Dtos/Generic/TokenData.cs
@@ -0,0 +1,6 @@
+﻿namespace AfiaNotebook.Authentication.Models.Dtos.Generic;
+public class TokenData
+{
+    public string JwtToken { get; set; }
+    public string RefreshToken { get; set; }
+}
--- AfiaNotebook.Authentication/Models/Dtos/Incoming/TokenRequestDto.cs
+++ AfiaNotebook.Authentication/Models/Dtos/Incoming/TokenRequestDto.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AfiaNotebook.Authentication.Models.Dtos.Outgoing;
-public class AuthResult
+namespace AfiaNotebook.Authentication.Models.Dtos.Incoming;
+public class TokenRequestDto
 {
+    [Required]
     public string JwtToken { get; set; }
+    
+    [Required]
     public string RefreshToken { get; set; }
-    public bool Success { get; set; }
-    public List<string> Errors { get; set; }
 }
