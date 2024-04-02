678e1141d08e92a7bc6d18f56f973b0d99096232
ObayIsm <obay.engineer@gmail.com>
2024-04-02 13:23:18 +0300

    Added a Constructor to the Error Class

--- AfiaNotebook.Entities/Dtos/Errors/Error.cs
+++ AfiaNotebook.Entities/Dtos/Errors/Error.cs
@@ -10,4 +10,11 @@ public class Error
     public int Code { get; set; }
     public string Type { get; set; }
     public string Message { get; set; }
+
+    public Error(int code, string type, string message)
+    {
+        Code = code;
+        Type = type;
+        Message = message;
+    }
 }
